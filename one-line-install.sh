5d98d3baecdb4c3d72b0f5a985d61129a9652c8b
WestleyK <westley@sylabs.io>
2018-08-28 12:02:41 -0700

    updated one line install script

--- one-line-install.sh
+++ one-line-install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # Created by: Westley K
 # email: westley@sylabs.io
-# Date: Aug 27, 2018
-# version-1.0.4
+# Date: Aug 28, 2018
+# version-1.0.5
 # https://github.com/WestleyK/rpi-backlight
 #
 # MIT License
@@ -17,26 +17,30 @@ INSTALL_PATH="/usr/local/bin/"
 KERNEL_VERSION="4.14.50-v7+"
 
 echo "Install script version:"
-echo "version-1.0.4"
+echo "version-1.0.5"
+echo 
 
 KERNEL=` uname -r `
 if [ "$KERNEL" != "$KERNEL_VERSION" ]; then
+    printf "\033[0;31mFATAL ERROR: \033[0m"
     echo "Kernel not supported!"
-    echo "Your kernel version: $KERNEL"
     echo "Supported kernel version: $KERNEL_VERSION"
+    echo "Your kernel version: $KERNEL"
     echo 
     echo "Install failed!"
     exit 1
 fi
 
-wget $SCRIPT_URL
+echo "Downloading file..."
+wget $SCRIPT_URL &> /dev/null
 mv rpi-backlight?raw=true $SCRIPT_NAME
 chmod +x $SCRIPT_NAME
-mv -f $SCRIPT_NAME $INSTALL_PATH
 
 echo 
 echo "########## rpi-backlight installed to /usr/local/bin/ ##########"
 echo
+echo ">> To finish the install, do:"
+echo " $ sudo mv rpi-backlight /usr/local/bin/"
 
 #
 # End install script
